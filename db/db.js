879b92a1188dd34924a36dcf14450863af1c5713
Yash Mandil <yashmandil@gmail.com>
2022-06-02 17:46:29 +0530

    initial commit

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
     //CATOSHI db
-    mongoose.connect('mongodb+srv://admin:pwd12345@cluster0.ptugc.mongodb.net/crogeBridgeTestnet?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}, (err) =>{
+    mongoose.connect('mongodb+srv://{user}:{password}@cluster0.ptugc.mongodb.net/crogeBridgeTestnet?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}, (err) =>{
         if(!err){
             console.log('MongoDB connected successfully')
         }
